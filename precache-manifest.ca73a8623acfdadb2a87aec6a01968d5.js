cbe56f9ed65964f8d607ecc5d62791ea240f5464
Niki May <nikkimay96@gmail.com>
2019-12-16 19:21:54 -0500

    Updates

---
+++ precache-manifest.ca73a8623acfdadb2a87aec6a01968d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5284ce7a1b46c4e1cce0101c787efd",
+    "url": "/ubiquitous/index.html"
+  },
+  {
+    "revision": "2cb9738485b393b2dfff",
+    "url": "/ubiquitous/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "98ecd7b193ecd34080fe",
+    "url": "/ubiquitous/static/js/2.a9554a8b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ubiquitous/static/js/2.a9554a8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cb9738485b393b2dfff",
+    "url": "/ubiquitous/static/js/main.b3576645.chunk.js"
+  },
+  {
+    "revision": "618e0f9db6e22f718d63",
+    "url": "/ubiquitous/static/js/runtime-main.22022c34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ubiquitous/static/media/logo.5d5d9eef.svg"
+  }
+]);
