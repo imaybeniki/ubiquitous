b83d784a9d28f735daf4dcdaa0c3cb8c916f3811
Niki May <nikkimay96@gmail.com>
2019-12-16 19:50:38 -0500

    adding folder structure

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src/js/pages/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/js/pages/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../scss/pages/module.scss';
+
+function App() {
+  return (
+    <div>
+      Welcome
+    </div>
+  );
+}
+
+export default App;
