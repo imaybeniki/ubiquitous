747c2465aa6d35bcd73dce48ef267d84f52e2cb8
Niki May <nikkimay96@gmail.com>
2019-12-21 17:26:14 -0500

    Start up project

--- src/js/pages/StartUp.js
+++ src/js/pages/StartUp.js
@@ -1,37 +1,21 @@
 import React, { Component } from 'react';
-import { TimelineLite } from 'gsap';
-import '../../scss/pages/startup.scss'
+import Crawl from 'react-star-wars-crawl';
 
 
 class StartUp extends Component {
-    constructor(props) {
-      super(props);
-      this.intro = React.createRef(); // shiny new React 16.3 ref API!
-    }
-    componentDidMount() {
-        const tl = new TimelineLite();
-        
-        tl
-          .to(this.intro.current, 4.5, { opacity: 1, delay: 1 })
-          .to(this.intro.current, 1.5, { opacity: 0 });
-    }
   
     render() {
       return (
-        <div className="container">
-          <section className="intro" ref={this.intro}>
-            <p>
-              A long time in the future, in a galaxy very, very near....
-              <br />
-              The company Natural Gene Editing has bought out Crispr, Google, Amazon.....
-              <br />
-              NGE provided every human a home, food to eat....
-              <br /> 
-              They have built all new humans in their own image, for a specific job....
-              <br />
-            
-            </p>
-          </section>
+        <div>
+          <Crawl 
+            title="Episode I"
+            subTitle="The Rise of NGE"
+            text="A long time in the future, in a galaxy very, very near....
+            The company Natural Gene Editing has bought out Crispr, Google, Amazon.....
+            As the environment soured, NGE offered quality code to protect your offspring from the treachorous environment....
+            NGE provided every human a bed, a job, food to eat....
+            They have built all new humans in their own image, for a specific job...."
+          />
         </div>
       );
     }
